e4d02649777de0c40b7ea6691ca3ee53adca6fe0
Rūta R <rutar4t@gmail.com>
2018-12-11 21:56:30 +0200

    matching funkcijoje apskaiciuoja kiek kiekvienonaudtojo atsakymu sutampa su prisijungusio naudotojo

--- functions.php
+++ functions.php
@@ -116,64 +116,47 @@ if ($conn->query($sql) === TRUE) {
 }
 };
 
-
+// BUTINAI!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! daug kintamuju i masyvus perkelt!!!!!!!!!!!!!!!!!!!!!!!!!!!
 function matching($curr_id){
 include("config.php");
 $conn = mysqli_connect($host, $user,$pass,$db); 
 $answers = mysqli_query($conn,"SELECT * FROM user_answers where user_id=$curr_id");
-$row = mysqli_fetch_array($answers); 
-$j=1;
-$k=1;
+$k=0;
+$curr_answer_id = array();
    while ($row = mysqli_fetch_array($answers)){
+   $curr_answer_id[]=$row['answer_id'];
+}
 
-   ${'curr_question'.$j}= $row['question_id']."<br>";
-   ${'curr_answer_id'.$j}=$row['answer_id']."<br>";
-   echo "curr_question turim";
-   echo ${'curr_question'.$j};
-   echo "curr_answer_id turim";
-   echo ${'curr_answer_id'.$j};
-   echo ".............<br>";
-   $j++;
-   }
+print_r($curr_answer_id);
 $users_all = mysqli_query($conn,"SELECT * FROM users");
 $rows_all = mysqli_num_rows($users_all);
 
 for ($i=1; $i <= $rows_all; $i++) { 
+    $rate[$i]=0;
 echo"<h1>...........KAI USER_ID YRA $i............</h1>";
 $user_answers = mysqli_query($conn,"SELECT * FROM user_answers where user_id=$i");
-$user_all = mysqli_fetch_array($user_answers);
-//visi atsakimai tam tikro naudotojo su id isfor ciklo  
+//$user_all = mysqli_fetch_array($user_answers);
+$n = 0 ;
+
   while ($user_all = mysqli_fetch_array($user_answers)){
-   ${'all_answer_id'.$k}=$user_all['answer_id'];
-   echo "atsakimo id  ".${'all_answer_id'.$k};
-   echo "<br>";
-   
-//if(${'curr_answer_id'.$k} == ${'all_answer_id'.$k}){
-echo"ar sutampa??? ";
-echo "<br>";
-echo ${'curr_answer_id5'};
-echo "ir ";
-echo ${'all_answer_id'.$k};
-echo "<br>";
-if(${'curr_answer_id5'} == ${'all_answer_id'.$k}){
-    echo"taip";
-    echo "<br>";
-}
-else{
-    echo"ne";
-    echo "<br>";
-}
-   echo "k = $k";
-   echo"<br>";
-   echo "............<br>";
-      $k++;
-     
-   
- 
-
-
-
+   $all_answer_id[$k]=$user_all['answer_id'];
+//echo "<br>";
+//echo $curr_answer_id[$n];
+settype( $curr_answer_id[$n],"integer");
+settype($all_answer_id[$k],"integer");
+//echo "ir ";
+//echo $all_answer_id[$k];
+//echo "<br>";
+if($curr_answer_id[$n] == $all_answer_id[$k]){
+    $rate[$i]++;
+    //echo"sutampa";
+    //echo"<br>";
 };
+
+      $k++;
+      $n++;
+};
+echo "<h2>".$rate[$i]."0%</h2>";
 };
  
     
